b203201a580790e055da01a284f84c1d18e28678
kamal9580 <nileshprince3118@gmail.com>
2024-08-03 00:06:39 +0530

    add filtering

--- src/filtering/01-ends-with.ts
+++ src/filtering/01-ends-with.ts
@@ -1,42 +1,42 @@
 
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient({
-//     log: ['info', 'query']
-// })
+const prisma = new PrismaClient({
+    log: ['info', 'query']
+})
 
-// async function main() {
-//   let res = await prisma.user.findMany({
-//     where: {
-//         email: {
-//           endsWith: 'gmail.com',
-//         },
-//         posts: {
-//           /// Has atleast one post published
-//           some: {
-//             published: true,
-//           },
-//         },
-//       },
-//       include: {
-//         posts: {
-//           where: {
-//             published: true,
-//           },
-//         },
-//       },
-//     })
-//     console.log(res);
+async function main() {
+  let res = await prisma.user.findMany({
+    where: {
+        email: {
+          endsWith: 'gmail.com',
+        },
+        posts: {
+          /// Has atleast one post published
+          some: {
+            published: true,
+          },
+        },
+      },//yha tak ka code dega all user jo end with gmail.com se hai jiska kuch published post hai
+      include: {
+        posts: {
+          where: {
+            published: true,
+          },//ye bol rha hai ki wo user ka all post jo wo user post kiya hai
+        },
+      },
+    })
+    console.log(res);
     
-// }
+}
 
-// main()
-//   .then(async () => {
-//     console.log("done");
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
+main()
+  .then(async () => {
+    console.log("done");
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
--- src/filtering/02-limits-and-offsets.ts
+++ src/filtering/02-limits-and-offsets.ts
@@ -1,25 +1,29 @@
 
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+import { skip } from 'node:test';
 
-// const prisma = new PrismaClient({
-//     log: ['info', 'query']
-// })
+const prisma = new PrismaClient({
+    log: ['info', 'query']
+})
 
-// async function main() {
-//   let res = await prisma.post.findMany({
-//     take: 3
-//   })
-//     console.log(res);
+async function main() {
+  let res = await prisma.post.findMany({
+    take: 3,
+    skip: 10
+  })
+    console.log(res);//ye jo take aur skip hai ye pagenation me madd karta hai ki
+    //take 3 and skip 10 ek page me 3 dikhao baki ko skip dusre page me dikhao
+    //isi ko gar sql me likhege to SELECT * FROM question OFFSET 0 LIMIT 10;
     
-// }
+}
 
-// main()
-//   .then(async () => {
-//     console.log("done");
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
+main()
+  .then(async () => {
+    console.log("done");
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
