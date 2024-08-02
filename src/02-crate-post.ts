6c311fe058bb47b34d20bdb77ccc0b4abeff3b89
kamal9580 <nileshprince3118@gmail.com>
2024-08-02 23:01:40 +0530

    add get and post

--- src/02-crate-post.ts
+++ src/02-crate-post.ts
@@ -1,28 +1,32 @@
 
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
-// async function main() {
-//   await prisma.post.create({
-//     data: {
-//      title: "title of post",
-//      author: {
-//         connect: {
-//             id: 1
-//         }
-//      }
-//     }
-//   })
-// }
+  
+// const prisma = new PrismaClient( {log: ['info' , 'query'],})
+//{log: ['info' , 'query'],} if we write this then it will show the what the sql command running in my prisma orm
 
-// main()
-//   .then(async () => {
-//     console.log("done");
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
+async function main() {
+  await prisma.post.create({
+    data: {
+     title: "title of post",
+     author: {
+        connect: {
+            id: 1 //we create the user table here we write 1 means this post connect to user 1
+        }
+     }
+    }
+  })
+}
+
+main()
+  .then(async () => {
+    console.log("done");
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
