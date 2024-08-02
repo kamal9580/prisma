35c0bea2724465e17b236c2882fe731412b8e447
kamal9580 <nileshprince3118@gmail.com>
2024-08-02 23:55:31 +0530

    add delte,udate

--- src/07-update-user.ts
+++ src/07-update-user.ts
@@ -1,26 +1,26 @@
 
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
-// async function main() {
-//   await prisma.user.update({
-//     where: {
-//         id: 1
-//     },
-//     data: {
-//         name: "harkiratsingh2"
-//     }
-//   })
-// }
+async function main() {
+  await prisma.user.update({
+    where: {
+        id: 1
+    },
+    data: {
+        name: "nileshkamal2"
+    }
+  })
+}
 
-// main()
-//   .then(async () => {
-//     console.log("done");
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
+main()
+  .then(async () => {
+    console.log("done");
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
--- src/08-delete-unpublished-posts.ts
+++ src/08-delete-unpublished-posts.ts
@@ -1,30 +1,30 @@
 
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
-// async function main() {
-//   await prisma.user.update({
-//     where: {
-//         id: 1
-//     },
-//     data: {
-//         posts: {
-//             deleteMany: {
-//                 published: false
-//             }
-//         }
-//     }
-//   })
-// }
+async function main() {
+  await prisma.user.update({
+    where: {
+        id: 1
+    },
+    data: {
+        posts: {
+            deleteMany: {
+                published: false
+            }
+        }
+    }
+  })
+}
 
-// main()
-//   .then(async () => {
-//     console.log("done");
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
+main()
+  .then(async () => {
+    console.log("done");
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
